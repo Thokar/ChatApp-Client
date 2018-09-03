9a0dba40df7ae9bc790ec10c6bb0a94ba33819ed
GARBSEN1\karbe <Thomas.karbe@Arvato.com>
2018-09-03 16:30:03 +0200

    .

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/toPromise'; not needed in A
 import { AngularFireAuth } from 'angularfire2/auth';
 import  * as firebase from 'firebase/app';
 
